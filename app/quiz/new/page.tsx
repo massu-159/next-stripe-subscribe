f8e10f4bb8e21842ff9728cc8e5161d592cc14fd
massu-159 <magjtwjp@gmail.com>
2023-10-01 09:24:14 +0900

    add quiz count

--- app/quiz/new/page.tsx
+++ app/quiz/new/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation"
 import { getAuthSession } from "@/lib/nextauth"
 import QuizNew from "@/components/quiz/QuizNew"
+import checkSubscription from "@/actions/checkSubscription"
+import getCount from "@/actions/getCount"
 
 // 新規クイズ作成ページ
 const QuizNewPage = async () => {
@@ -11,7 +13,11 @@ const QuizNewPage = async () => {
     redirect("/login")
   }
 
-  return <QuizNew userId={session.user.id} />
+  // サブスク有効期限
+  const isSubscription = await checkSubscription({userId: session.user.id})
+  // クイズ生成回数
+  const count = await getCount({userId: session.user.id})
+  return <QuizNew userId={session.user.id} isSubscription={isSubscription} count={count} />
 }
 
 export default QuizNewPage
